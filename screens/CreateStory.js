8b55b3d6636189c5c5ec30a4117bf2b96b92b5e3
Meher <82945138+Meher450@users.noreply.github.com>
2021-10-01 23:00:35 +0530

    Add files via upload

---
+++ screens/CreateStory.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+export default class CreateStory extends Component{
+  render(){
+    return(
+      <View style={styles.container}>
+      </View>
+    )
+  }
+}
+
+const styles=StyleSheet.create({
+  container:{
+    flex:0.8,
+    backgroundColor:'green',
+  }
+})
